ccceb758e89f57d2ffc524a9118c0be367bc0f78
RPLKillen <rpl@example.com>
2017-06-07 14:56:17 +0200

    Add new tone alarm

--- src/drivers/stm32/tone_alarm/tone_alarm.cpp
+++ src/drivers/stm32/tone_alarm/tone_alarm.cpp
@@ -421,6 +421,7 @@ ToneAlarm::ToneAlarm() :
 	_default_tunes[TONE_BARO_WARNING_TUNE] = "MFT255L4gf#fed#d";				// baro warning
 	_default_tunes[TONE_SINGLE_BEEP_TUNE] = "MFT100a8";                             // single beep
 	_default_tunes[TONE_HOME_SET] = "MFT100L4>G#6A#6B#4";
+	_default_tunes[TONE_YEE] = "MFMLL8T120>cd.cMSe.e.MLdc.d...MSgg...MLcd.cMSe.e.MLf.gN20........";	// yee
 
 	_tune_names[TONE_STARTUP_TUNE] = "startup";			// startup tune
 	_tune_names[TONE_ERROR_TUNE] = "error";				// ERROR tone
@@ -437,6 +438,7 @@ ToneAlarm::ToneAlarm() :
 	_tune_names[TONE_BARO_WARNING_TUNE] = "baro_warning";			// baro warning
 	_tune_names[TONE_SINGLE_BEEP_TUNE] = "beep";                    // single beep
 	_tune_names[TONE_HOME_SET] = "home_set";
+        _tune_names[TONE_YEE] = "yee"; 					// yee
 }
 
 ToneAlarm::~ToneAlarm()
