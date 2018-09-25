451e9e007afc3bd5623c8bef47f0f813b434b036
Ludvig Ericson <ludvig@lericson.se>
2018-09-25 10:18:37 +0200

    Check error message for nullity

--- src/lib/FlightTasks/tasks/FlightTask/Error.hpp
+++ src/lib/FlightTasks/tasks/FlightTask/Error.hpp
@@ -4,7 +4,7 @@ struct Error {
 	int num;
 	const char *msg;
 	Error(int num_, const char *msg_) : num{num_}, msg{msg_} {}
-	Error(const char *msg_) : num{-1}, msg{msg_} {}
+	Error(const char *msg_) : num{msg_ == nullptr ? 0 : -1}, msg{msg_} {}
 	Error() : num{0}, msg{"success"} {}
 	Error(bool ok) : num{ok ? 0 : -1}, msg{"success"} {}
 	operator const char *() { return msg; }
